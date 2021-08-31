690c91eb5bd8bd7712ac854f22b01b3ed86fb4a2
Dan Cassin <dancassin@gmail.com>
2021-08-30 20:17:30 -0700

    initial commit; creating separate folder

---
+++ trading_stats_dashboard.py
@@ -0,0 +1,147 @@
+import pandas as pd
+import numpy as np
+import datetime
+import json
+
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from pandas.io.formats import style
+
+import plotly.graph_objs as go
+import plotly.express as px
+
+
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX])
+
+
+# -----------------------------------------------------------
+# Constants
+today = pd.to_datetime(datetime.date.today())
+
+year = today.year
+
+ytd = pd.to_datetime(f'01-01-{year}')
+
+t_minus_30 = today - datetime.timedelta(30)
+
+t_minus_90 = today - datetime.timedelta(90)
+
+ttm = today - datetime.timedelta(365)
+
+time_periods = {1:ttm, 2:ytd, 3:t_minus_90, 4:t_minus_30}
+
+# -----------------------------------------------------------
+# Data Processing
+
+FILEPATH = '/Users/DanCassin/Development/python/minervini_stock_selector/TTM_tax_lots_083021.csv'
+
+tax_lot_df = pd.read_csv(FILEPATH,
+            usecols=[
+                'Symbol', 'Quantity', 'Opening Date', 'Cost/Share $', 'Total Cost $',
+                'Closing Date',	'Price/Share $', 'Proceeds $', 'Gain $'],
+            skiprows=13,
+            delimiter=',',
+            skipfooter=2,
+            engine='python'
+)
+
+# column formatting and selection
+tax_lot_df.rename(columns = lambda x: x.lower().replace('$', '').strip().replace(' ','_'), inplace=True)
+
+# replacing double dashes in symbol column with NaN and forward filling the stock ticker
+tax_lot_df = tax_lot_df.replace('--', np.nan)
+tax_lot_df['symbol']= tax_lot_df['symbol'].str.strip()
+tax_lot_df['symbol'].replace('Sell', np.nan, inplace=True)
+tax_lot_df['symbol'] = tax_lot_df['symbol'].ffill()
+
+tax_lot_df.dropna(subset=['opening_date'], inplace=True)
+
+tax_lot_df['opening_date'] = tax_lot_df['opening_date'].astype('Datetime64')
+tax_lot_df['closing_date'] = tax_lot_df['closing_date'].astype('Datetime64')
+
+tax_lot_df['pct_gain/loss'] = round((tax_lot_df['gain'] / tax_lot_df['total_cost']) * 100, 2)
+
+# # -----------------------------------------------------------
+# # Layout
+app.layout = dbc.Container([
+    html.Div([
+        html.Br(),
+        html.H1('Trading Performance', style={'align':'left'}),
+        ]
+    ),
+    html.Div([
+        html.Br(),
+        dcc.Slider(
+            id='time_slider',
+            min=1,
+            max=4,
+            step=None,
+            marks={
+                1 : 'TTM',
+                2 : 'YTD',
+                3 : '90 Days',
+                4 : '30 Days',
+            },
+            value=2,
+        ),
+        
+    ]),
+    #html.Div(id='slider-drag-output'),
+    html.Br(),
+    html.Div(
+        [
+            dbc.Row(
+                [
+                    dbc.Col(
+                        dcc.Graph(id='gain_loss_hist'),
+                        width={'size':4},
+                    ),
+                    dbc.Col(
+                        dcc.Graph(id='gain_hist'),
+                        width={'size':4},
+                    ),
+                    dbc.Col(
+                        dcc.Graph(id='loss_hist'),
+                        width={'size':4},
+                    ),
+                ]
+            )
+        ]
+    ),
+dcc.Store(id='sub_df')
+])
+
+# -----------------------------------------------------------
+# Callbacks
+@app.callback(Output('sub_df','data'),[Input('time_slider','value')])
+def setting_date_filter(selected_time):
+    sub_df = tax_lot_df[tax_lot_df['closing_date'] >= time_periods[selected_time]]
+
+    return sub_df.to_json()
+
+@app.callback(Output('gain_loss_hist', 'figure'), [Input('sub_df', 'data')])
+def gain_loss_hist(jsonified_cleaned_data):
+
+    df = pd.read_json(jsonified_cleaned_data)
+
+    fig = px.histogram(
+        df, 
+        x='pct_gain/loss',
+        nbins=50,
+        labels={'x':'% Gain of Loss', 'y':'Trade Count'})
+
+
+    return fig
+
+
+
+# -----------------------------------------------------------
+if __name__ == "__main__":
+    app.run_server(debug=True)
